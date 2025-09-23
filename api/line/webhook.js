4993713bc7f2a99204491acd0880dbceda06bb29
cypheroneinc-oss <cypherone.inc@gmail.com>
2025-09-23 15:35:15 +0900

    Update and rename webhook.ts to webhook.js

---
+++ api/line/webhook.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST');
+    return res.status(405).end('Method Not Allowed');
+  }
+  return res.status(200).json({ message: 'OK' });
+}
